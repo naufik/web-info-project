8a75d24400fe4ca5f9b2993c779bb1413eab6ef6
Naufal <nauffik@gmail.com>
2018-03-27 02:15:24 +1100

    angular frontend sample

---
+++ angular-workspace/src/app/soon/soon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'soon',
+  templateUrl: './soon.component.html',
+  styleUrls: ['./soon.component.css']
+})
+export class SoonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
