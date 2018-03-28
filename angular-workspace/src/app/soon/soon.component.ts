a9313f15bff104ba1607acb3335a59abb401275d
Naufal <nauffik@gmail.com>
2018-03-29 02:11:23 +1100

    improved the website by a bit

--- angular-workspace/src/app/soon/soon.component.ts
+++ angular-workspace/src/app/soon/soon.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 interface UserData {
 	name: string,
 	github?: string
-};
+}
 
 @Component({
 	selector: 'soon',
@@ -14,10 +14,21 @@ export class SoonComponent implements OnInit {
 
   ngOnInit(): void {
     this.randomize();
-    setInterval(() => {this.randomize()}, 4000)
+    setInterval(() => {this.animate()}, 1000)
   }
 
   constructor() { }
+
+  animate() {
+    if (this.dots === "...") {
+      this.randomize();
+      this.dots = "";
+    } else {
+      this.dots += "."
+    }
+  }
+
+  dots = "";
 
 	displayMembers = false;
 
@@ -32,9 +43,48 @@ export class SoonComponent implements OnInit {
   // ]
 
   taglines: string[] = [
-    "Still cooking, please wait.",
-    "Hold on there! We're not done yet.",
-    "Broken fridges used to be cool!"
+    "Still cooking, please wait",
+    "Preparing",
+    "Bringing a local grocery helper to your neighborhood",
+    "Making groceries easy",
+    "Making groceries easy for you",
+    "Revolutionizing the grocery shopping experience",
+    "Trying to understand JavaScript promises",
+    "Under construction",
+    "Trying to reduce global food waste problem",
+    "Judging people who don't finish food in restaurants",
+    "Indexing best-before dates",
+    "Please come back soon! We're not done yet",
+    "Throwing away broken fridges that used to be cool",
+    "Coming to a neighborhood near you",
+    "Packing our stuff",
+    "Arguing whether WebStorm is better than Sublime",
+    "Being taken to Medical Building rather than Redmond Barry by muscle memory",
+    "Standing in a line in STOP1",
+    "Trying to think of a good commit message",
+    "Thinking of a good Kahoot display name",
+    "Acummulating bonus points",
+    "Reorganizing the fridge",
+    "Extending 15-minute interviews to 1 hour",
+    "Attending tutorials",
+    "Trying to enrol in obsolete Bachelor of Engineering",
+    "Forcing my groupmates to npm install prettier",
+    "Going to free pizzas rather than eating leftover foods",
+    "Throwing away leftovers",
+    "Teaching food how to code in CoffeeScript",
+    "Wishing team 51 a nice day",
+    "Regretting our group name because we thought of a better one",
+    "Deciding who should pitch the idea in the last workshop",
+    "Skipping other subjects to do INFO30005",
+    "Trying to help you shop",
+    "Doing some math with CSS paddings",
+    "Bringing the future of groceries in the future",
+    "Sneaking in the commerce faculty to look for someone to sell this",
+    "Reminding you all to drink enough water",
+    "Trying to escape callback hell",
+    "Learning JavaScript from W3Schools",
+    "Submitting deliverables",
+    "Resolving merge conflicts in package-lock.json"
   ]
 
 	color = "blue";
@@ -67,6 +117,11 @@ export class SoonComponent implements OnInit {
 	}
 
 	randomize() {
- 	  this.taglineIndex = Math.floor(Math.random() * (this.taglines.length - 0.01));
+ 	  let rand = () => { return Math.floor(Math.random() * (this.taglines.length - 0.01))))};
+    let r = rand();
+    while (r === this.taglineIndex) {
+      r = rand();
+    }
+    this.taglineIndex = r === this.taglineIndex ? r : r - 1;
   }
 }
