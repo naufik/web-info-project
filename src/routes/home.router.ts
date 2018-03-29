61900504143287c8d4bf810df3c8a1d71868ce85
Harimau Kumbang <fdarma@student.unimelb.edu.au>
2018-03-29 16:16:00 +1100

    server

--- src/routes/home.router.ts
+++ src/routes/home.router.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response } from 'express';
 import * as Express from 'express';
-
+import * as Path from 'path';
 
 const router = Router();
 
 /** write your router configuration here */
 
-router.use("/", Express.static())
+router.use("/", Express.static(Path.join(__dirname,"../..", "views")));
 /** exports the router */
 export default router;
