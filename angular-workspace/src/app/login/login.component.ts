350ecd18cf269afbe9c14e3f63c118e44132fef1
Harimau Kumbang <fdarma@student.unimelb.edu.au>
2018-04-15 16:31:15 +1000

    added some ts to login

--- angular-workspace/src/app/login/login.component.ts
+++ angular-workspace/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  @Output()
+  submit: EventEmitter<any>;
+
+  constructor() {
+    this.submit = new EventEmitter<any>();
+  }
 
   ngOnInit() {
+  }
+
+  login(username: string) {
+    this.submit.emit(username);
   }
 
 }
