e381b928736eea91c19735ac2c0365b54f6d9207
Naufal <nauffik@gmail.com>
2018-04-15 18:30:03 +1000

    WIP still doing that food component

--- angular-workspace/src/app/fooditem/fooditem.component.ts
+++ angular-workspace/src/app/fooditem/fooditem.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+interface FoodItem {
+  imgSrc: string,
+  name: string,
+  expiry: Date
+}
 
 @Component({
-  selector: 'app-fooditem',
+  selector: 'food-item',
   templateUrl: './fooditem.component.html',
   styleUrls: ['./fooditem.component.css']
 })
 export class FooditemComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  height: number = 300;
+  @Input()
+  width: number = 300;
+  @Input()
+  food: FoodItem;
+  @Output()
+  click: EventEmitter<FoodItem>;
+
+  constructor() {
+    this.click = new EventEmitter<FoodItem>;
+  }
+
+  onClick() {
+    this.click.emit(this.food);
+  }
 
   ngOnInit() {
   }
 
+  daysToDue() {
+    return (this.food.expiry.valueOf() - (new Date()).valueOf());
+  }
 }
