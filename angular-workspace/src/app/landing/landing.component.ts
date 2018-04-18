caafc887e6b0eeb1e19937567f9e108f088528ca
Harimau Kumbang <fdarma@student.unimelb.edu.au>
2018-04-18 14:54:33 +1000

    edited landing to look nice

--- angular-workspace/src/app/landing/landing.component.ts
+++ angular-workspace/src/app/landing/landing.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule, ActivatedRoute } from '@angular/router'
 
+interface UserData {
+  name: string,
+  github?: string
+}
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
@@ -25,5 +30,29 @@ export class LandingComponent implements OnInit {
     this.testvar += "A";
     this.router.navigate(['/main']);
   }
+
+  collapsearr = [true, true];
+
+  collapse(menu: number){
+    this.collapsearr[menu] = !this.collapsearr[menu];
+  }
+
+  public getGithubLink (username: string): string {
+    return "http://github.com/" + username;
+  }
+  public members: UserData[] = [
+    {
+      name: "Naufal Fikri Setiawan",
+      github: "naufik"
+    },
+    {
+      name: "Farhan Darma",
+      github: "fdarma"
+    },
+    {
+      name: "Manindra Arora",
+      github: "maniarora"
+    }
+  ];
 
 }
