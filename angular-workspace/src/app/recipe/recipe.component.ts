ae4803633d085cc188a5b72bb0eae0b3714d5776
Naufal <nauffik@gmail.com>
2018-04-19 02:06:16 +1000

    WIP modularize recipe item

--- angular-workspace/src/app/recipe/recipe.component.ts
+++ angular-workspace/src/app/recipe/recipe.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DataRetrieverService, List } from '../dataretriever.service';
 
 @Component({
   selector: 'app-recipe',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeComponent implements OnInit {
 
+  @Input()
+  listdata: List;
+
   constructor() { }
 
   ngOnInit() {
+  
   }
 
 }
