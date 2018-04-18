55769adfd28b31fe8d28a01b87372f26430827c6
Naufal <nauffik@gmail.com>
2018-04-19 03:10:40 +1000

    RHS groceries done

--- angular-workspace/src/app/grocery-list/grocery-list.component.ts
+++ angular-workspace/src/app/grocery-list/grocery-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { DataRetrieverService, List, FridgeFood, ListFood } from '../dataretriever.service';
+
 
 @Component({
   selector: 'app-grocery-list',
   templateUrl: './grocery-list.component.html',
-  styleUrls: ['./grocery-list.component.css']
+  styleUrls: ['./grocery-list.component.css'],
+  providers: [ DataRetrieverService ]
 })
 export class GroceryListComponent implements OnInit {
 
-  constructor() { }
+  shoppingList = ["Raw Salmon", "Chicken Breast", "Full-Cream Milk", "Coffee Ground", "Wholegrain Bread"];
+  appendableLists: List[] = [];
+
+  private service: DataRetrieverService;
+
+  constructor(ds: DataRetrieverService) {
+    this.service = ds;
+  }
 
   ngOnInit() {
+    this.appendableLists = this.service.getLists();
+  }
+
+  appendList() {
+    
   }
 
 }
