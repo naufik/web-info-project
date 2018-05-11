1d1ad4717be55c4e7929f14faaf24ae6492fc5e1
Naufal <nauffik@gmail.com>
2018-05-11 23:54:13 +1000

    all grocery list interaction now works

--- angular-workspace/src/app/grocery-list/grocery-list.component.ts
+++ angular-workspace/src/app/grocery-list/grocery-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { DataRetrieverService, List, FridgeFood, ListFood } from '../dataretriever.service';
 
 
@@ -10,10 +10,13 @@ import { DataRetrieverService, List, FridgeFood, ListFood } from '../dataretriev
 })
 export class GroceryListComponent implements OnInit {
 
+  @Input()
+  userEmail: string = "john@john.com";
+
   checks = {};
 
-  shoppingList = ["Raw Salmon", "Chicken Breast", "Full-Cream Milk", "Coffee Ground", "Wholegrain Bread"];
-  appendableLists: List[] = [];
+  shoppingList = [];
+  appendableLists: any[] = [];
 
   private service: DataRetrieverService;
 
@@ -22,21 +25,48 @@ export class GroceryListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.appendableLists = this.service.getLists();
-    for (let item of this.appendableLists) {
-      this.checks[item.name] = false;
-    }
+    this.refresh();
   }
 
   appendLists() {
-    let tempLists = this.appendableLists.filter(i => this.checks[i.name]);
+    console.log(this.checks);
+    let tempLists = this.appendableLists.filter(i => this.checks[i._id]);
+
     for (let currentList of tempLists) {
       for (let shopItem of currentList.contents) {
-        if (!this.shoppingList.includes(shopItem.name)){
-          this.shoppingList.push(shopItem.name);
+        if (!this.shoppingList.map(thing => thing.name).includes(shopItem.name)){
+          this.shoppingList.push(shopItem);
         }
       }
     }
+
+    this.service.saveGroceries(this.userEmail, this.shoppingList).then(() => {
+      this.refresh();
+    });
+  }
+
+  refresh() {
+    this.service.getUserData(this.userEmail).then((data: { groceries: any[] }) => {
+      this.shoppingList = data.groceries;
+    });
+
+    this.service.getListsForUser(this.userEmail).then((data: any[]) => {
+      this.appendableLists = data;
+      for (let item of this.appendableLists) {
+        if (!this.checks[item._id]){
+          this.checks[item._id] = false;
+        }
+      }
+    });
+  }
+
+  toFridge() {
+    this.service.getUserData(this.userEmail).then((data: { fridge: any[] }) => {
+      return this.service.saveFridge(this.userEmail, 
+        data.fridge.concat(this.shoppingList));
+    }).then(() => {
+      console.log("AYAYAYAYAYAY");
+    })
   }
 
 }
