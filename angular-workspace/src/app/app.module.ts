c8257e8e0153997e26ae27c90a944cbbfe5b556a
Naufal <nauffik@gmail.com>
2018-05-11 17:29:52 +1000

    enabled ngModel

--- angular-workspace/src/app/app.module.ts
+++ angular-workspace/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SoonComponent } from './soon/soon.component';
@@ -62,7 +63,8 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [RouterModule],
   bootstrap: [AppComponent]
