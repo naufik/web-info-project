0d3d48675bc5d55dfc6c4c2f3fc3ebf8b1959be2
Naufal Fikri <nauffik@gmail.com>
2018-05-28 01:59:18 +1000

    Revert "frontend changes to mimic new authentication"
    This reverts commit 2c3b443fe017ce7740257f0dd1ad923cc7a2f6e5.

--- angular-workspace/src/app/dataretriever.service.ts
+++ angular-workspace/src/app/dataretriever.service.ts
@@ -4,6 +4,127 @@ import { HttpClient } from '@angular/common/http';
 /** GET URL */
 const API_URL = "http://to-dd.herokuapp.com/api/"
 
+/** placeholder hardcoded data */
+const allLists: List[] = [
+{
+  name: "Tuna Sandwich",
+  url: "tuna",
+  contents: [{
+    name: "Tuna",
+    qty: 2.5,
+    unit: "kg+s"
+  },
+  {
+    name: "Bread",
+    qty: 2
+  },
+  {
+    name: "Tomatoes",
+    qty: 0.5,
+    unit: "g+s"
+  },
+  {
+    name: "Cheese",
+    qty: 2,
+    unit: "slice+s"
+  },
+  {
+    name: "Lettuce",
+    qty: 3,
+    unit: "slice+s"
+  }]
+},
+{
+  name: "Kids Favorites",
+  url: "kids-favs",
+  contents: [{
+    name: "Sausages",
+    qty: 3,
+    unit: "kg+s"
+  },
+  {
+    name: "Beans",
+    qty: 5,
+    unit: "can+s"
+  },
+  {
+    name: "Cereal",
+    qty: 2,
+    unit: "package+s"
+  },
+  {
+    name: "Milk",
+    qty: 1,
+    unit: "carton+s"
+  },
+  {
+    name: "Broccoli",
+    qty: 20,
+    unit: "slice+s"
+  }]
+}, {
+  name: "Not for the Kids",
+  url: "alcohol",
+  contents: [{
+    name: "Wine",
+    qty: 2,
+    unit: "bottle+s"
+  }, {
+    name: "Whisky",
+    qty: 1,
+    unit: "bottle+s"
+  }]
+}
+]
+
+const allFridge: FridgeFood[] = [
+{
+  name: "Milk",
+  qty: 3,
+  expiry: new Date(new Date().setDate((new Date().getDate()) + 4))
+},
+{
+  name: "Snappers",
+  qty: 2,
+  expiry: new Date(new Date().setDate((new Date().getDate()) + 3))
+},
+{
+  name: "Beef",
+  qty: 1,
+  unit: "kg+s",
+  expiry: new Date(new Date().setDate((new Date().getDate()) + 7))
+},
+{
+  name: "Eggs",
+  qty: 5,
+  unit: "pc+s",
+  expiry: new Date(new Date().setDate((new Date().getDate()) + 1))
+}
+]
+
+export interface List {
+  _id?: any,
+  name: string,
+  url: string,
+  contents: ListFood[]
+}
+
+export interface ListFood {
+  _id?: any,
+  name: string,
+  qty: number,
+  unit?: string, // Unit of measurement (pcs, grams, kgs, litres)
+  expiry?: Date
+}
+
+export interface FridgeFood {
+  _id?: any,
+  name: string,
+  qty: number,
+  unit?: string, // Unit of measurement (pcs, grams, kgs, litres)
+  expiry: Date
+}
+
 @Injectable()
 export class DataRetrieverService {
 
@@ -11,6 +132,22 @@ export class DataRetrieverService {
 
   constructor(http: HttpClient) {
     this.http = http;
+  }
+
+  public getLists() {
+    return allLists;
+  }
+
+  public getFridge() {
+    return allFridge;
+  }
+
+  public addData(data: FridgeFood) {
+    allFridge.push(data);
+  }
+
+  public addNewList(data: List) {
+    allLists.push(data);
   }
 
   public getUserData(email: string) {
@@ -133,18 +270,6 @@ export class DataRetrieverService {
           } else {
             reject(new Error("Unknown Error"));
           }
-        }
-      });
-    })
-  }
-
-  public getSesionData() {
-    return new Promise((resolve, reject) => {
-      this.http.get(API_URL + "user/session/").subscribe((data: any) => {
-        if (data) {
-          resolve(data);
-        } else {
-          reject(new Error("Unknown Error"));
         }
       });
     })
