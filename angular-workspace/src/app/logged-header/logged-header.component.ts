b3f743d16ff85630dc8cc99e54eeb10903939763
Naufal Fikri <nauffik@gmail.com>
2018-05-28 02:05:56 +1000

    session data is now stored properly

--- angular-workspace/src/app/logged-header/logged-header.component.ts
+++ angular-workspace/src/app/logged-header/logged-header.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { DataRetrieverService } from '../dataretriever.service';
 
 @Component({
   selector: 'app-logged-header',
   templateUrl: './logged-header.component.html',
-  styleUrls: ['./logged-header.component.css']
+  styleUrls: ['./logged-header.component.css'],
+
 })
 export class LoggedHeaderComponent implements OnInit {
 
-  constructor() { }
+  private dataRetriever: DataRetrieverService;
+  private user: any;
+
+  constructor(ds: DataRetrieverService) {
+    this.dataRetriever = ds;
+  }
 
   ngOnInit() {
+    this.dataRetriever.getSessionData().then((data) => {
+      this.user = data;
+    });
   }
 
 }
